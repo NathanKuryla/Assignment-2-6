858b80e4fb85fd9005afaeef3d1d1a4e476683cc
Nathan Kuryla <nathan.kuryla@gmail.com>
2019-10-06 16:50:31 -0500

    Low Income Changes

--- SAInvoiceSystem/src/com/datacontainers/LowIncome.java
+++ SAInvoiceSystem/src/com/datacontainers/LowIncome.java
@@ -1,12 +1,21 @@
 package com.datacontainers;
 
 public class LowIncome extends Customer {
+	
+	private double tax;
+	private double fee;
 
 	public LowIncome(String customerCode, String customerType, Person contact, String name, Address address) {
 		super(customerCode, customerType, contact, name, address);
 	}
 	
+	public double getSub() {
+		
+	}
+	
 	public double getTax() {
+		tax = 0.00;
+		return tax;
 	}
 	
 	public double getDiscount() {
@@ -14,7 +23,8 @@ public class LowIncome extends Customer {
 	}
 	
 	public double getAdditionalFee() {
-		
+		fee = 50.75;
+		return fee;
 	}
 	
 	
