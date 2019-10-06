3acf5898937cc41b61079c755a6c9fa5db0ec3e9
Nathan Kuryla <nathan.kuryla@gmail.com>
2019-10-06 16:36:58 -0500

    General Updates

--- SAInvoiceSystem/src/com/datacontainers/General.java
+++ SAInvoiceSystem/src/com/datacontainers/General.java
@@ -1,20 +1,29 @@
 package com.datacontainers;
 
 public class General extends Customer {
+	
+	private double tax;
+	private double discount;
+	private double fee;
 
 	public General(String customerCode, String customerType, Person contact, String name, Address address) {
 		super(customerCode, customerType, contact, name, address);
 	}
 	
+	@Override
 	public double getTax() {
+		return tax;
 	}
 	
+	@Override
 	public double getDiscount() {
-		
+		discount = 0.0;
+		return discount;
 	}
 	
+	@Override
 	public double getAdditionalFee() {
-		
+		fee = 0.0;
+		return fee;
 	}
-	
 }
